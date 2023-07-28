3613f37fed8ed3b4bbf039bafc00c90277451594
Sunny <98167266+Sunnnyyy16@users.noreply.github.com>
2023-07-28 11:33:00 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -5,14 +5,14 @@ visited=[0]*(n+1)
 
 for i in range(v): 
     a,b=map(int,input().split())
-    graph[a]+=[b] 
-    graph[b]+=[a] 
+    graph[a].append(b) 
+    graph[b].append(a)
     
-def dfs(v):
-    visited[v]=1
-    for nx in graph[v]:
-        if visited[nx]==0:
-            dfs(nx)
+def dfs(start):
+    visited[start]=1
+    for i in graph[start]:
+        if visited[i]==0:
+            dfs(i)
             
 dfs(1)
 print(sum(visited)-1)
